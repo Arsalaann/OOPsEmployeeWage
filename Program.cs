ad761956c71c5cf174865fec51171ffc4f63ad55
Arsalaann <arsalankhanxk@gmail.com>
2023-05-06 14:53:59 +0530

    Wage for month

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿namespace OOPsEmployeeWage{
     class Program{
-        private int WAGEPERHOUR=20,FULLTIME=8,PARTTIME=8;
+        private int WAGEPERHOUR=20,FULLTIME=8,PARTTIME=8,MONTH=20;
         private int DailyWage(int d,int wph){
             return d*FULLTIME*WAGEPERHOUR;
+        }
+        private int DailyWage(int m,int d,int wph){
+            return m*MONTH*d*FULLTIME*WAGEPERHOUR;
         }
         public static void Main(string[] args){
             Program obj=new Program();
             Attendance employee=new Attendance();
             employee.CheckAttendance();
             obj.DailyWage(1,obj.WAGEPERHOUR);
+            obj.DailyWage(1,1,obj.WAGEPERHOUR);
         }
     }
 }
