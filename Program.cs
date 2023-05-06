ff987e59914d4b7357aabf52bc17bb05175dfc5d
Arsalaann <arsalankhanxk@gmail.com>
2023-05-06 14:45:05 +0530

    Daily wage

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿namespace OOPsEmployeeWage{
     class Program{
+        private int WAGEPERHOUR=20,FULLTIME=8;
+        private int DailyWage(int d,int wph){
+            return d*FULLTIME*WAGEPERHOUR;
+        }
         public static void Main(string[] args){
+            Program obj=new Program();
             Attendance employee=new Attendance();
             employee.CheckAttendance();
+            obj.DailyWage(1,obj.WAGEPERHOUR);
         }
     }
 }
